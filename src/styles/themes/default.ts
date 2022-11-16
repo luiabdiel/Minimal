7db708f14f6a27e4148418d3662c8a56b19505da
Luiggi Abdiel <luiggiabdieldev@gmail.com>
2022-11-16 18:15:59 -0300

    feat: visual structure of the project

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,14 @@
+export const defaultTheme = {
+    white: '#FFF',
+
+    'blue-500': '#606CCB',
+    'blue-100': '#8F95B2',
+    'black-900': '#000000',
+    'back-700': '#333333',
+    'gray-500': '#828282',
+    'gray-100': 'rgba(218, 218, 218, 0.4);',
+    'yellow-700': '#CB8A14',
+    'yellow-100': '#FFFAF1',
+    'green-500': '#359A73',
+    'green-100': '#F5FBF8',
+} as const
