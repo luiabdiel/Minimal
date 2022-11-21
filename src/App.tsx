9aef06eb36d2e0c4482a8bd75cde7016f542d418
Luiggi Abdiel <luiggiabdieldev@gmail.com>
2022-11-21 20:14:39 -0300

    react router dom in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
 import { ThemeProvider } from 'styled-components'
-import { TransactionsProvider } from './contexts/TransactionsContext'
-import { Transactions } from './pages/Transactions'
+import { Home } from './pages/Home'
+// import { TransactionsProvider } from './contexts/TransactionsContext'
+// import { Transactions } from './pages/Transactions'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
+import { Transactions } from './pages/Transactions'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-      <TransactionsProvider>
-        <Transactions />
-      </TransactionsProvider>
+      {/* <TransactionsProvider> */}
+        {/* <Transactions /> */}
+        <Router>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/transactions' element={<Transactions />} />
+          </Routes>
+        </Router>
+      {/* </TransactionsProvider> */}
     </ThemeProvider>
   )
 }
