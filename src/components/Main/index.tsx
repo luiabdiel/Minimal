b79e3ef8ec766cafda75b6fe9e94ff8c507cb26f
Luiggi Abdiel <luiggiabdieldev@gmail.com>
2022-11-21 20:18:38 -0300

    translation of information

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -9,7 +9,7 @@ export function Main() {
         <MainContainer>
             <MainContentText>
                 <h1>Sistema para controlefinanceiro online</h1>
-                <p>Conheça a <span>Minimal</span> e coloque as estratégias organizacionais em ação de maneira simples, sólida e assertiva.</p>
+                <p>Conheça a <span>Minimal</span> e coloque as estratégias organizacionais de gestão financeira em ação de maneira simples, sólida e assertiva.</p>
                 <button><Link to={'/transactions'}>Painel de gestão</Link></button>
             </MainContentText>
             <MainContentImage>
