80a0d5b947cad734b1ab1a3926feab61e6a22ead
Luiggi Abdiel <luiggiabdieldev@gmail.com>
2022-12-09 20:53:39 -0300

    feat: new page login static

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,31 +1,38 @@
-import { useState } from 'react'
-import BackgroundSvg from '../../assets/AnimatedShape.svg'
-import { FormContainer, LoginContainer, LoginPage } from './styles'
+import React from "react";
+import { Link } from "react-router-dom";
+import { Button } from "../../components/Button";
+import { Input } from "../../components/Input";
+import { FormContainer, LoginContainer, SubContainerSign } from "./styles";
 
-export function Login () {
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
+export const Login = () => {
+    const handleChange = () => {
+        console.log('digitando...')
+    }
 
     return (
-        <LoginPage>
-            <img src={BackgroundSvg} alt="" />
-            <LoginContainer>
-                <h1>Acessar o sistema</h1>
-                <FormContainer>
-                    <label>Usuário</label>
-                    <input 
-                        type="text" 
-                        value={email} 
-                        onChange={(e) => setEmail(e.target.value)} 
-                    />
-                    <label>Senha</label>
-                    <input 
-                        type="password" 
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                    <button type='submit'>Entrar</button>
-                </FormContainer>
-            </LoginContainer>
-        </LoginPage>
+        <LoginContainer>
+            <FormContainer>
+                <h1>Faça o seu Login</h1>
+                <Input 
+                    placeholder="Digite o seu email"
+                    name="email"
+                    type="email"
+                    onChange={handleChange}
+                />
+                <Input 
+                    placeholder="Digite a sua senha"
+                    name="password"
+                    type="password"
+                    onChange={handleChange}
+                />
+                <Button 
+                    type="submit"
+                />
+                <SubContainerSign>
+                    <p>Não possui conta?</p>
+                    <Link to="/register">Cadastrar</Link>
+                </SubContainerSign>
+            </FormContainer>
+        </LoginContainer>
     )
 }
--- src/pages/Login/styles.ts
+++ src/pages/Login/styles.ts
@@ -1,96 +1,49 @@
 import styled from "styled-components";
 
-export const LoginPage = styled.div`
-
-    height: 100vh;
-    width: 100vw;
-    
-    position: fixed;
-    
-    img {
-        background-size: cover;
-        background-position: center;
-
-        min-height: 100%;
-        min-width: 100%;
-    }
-`
-
 export const LoginContainer = styled.div`
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
+    height: 100vh;
+    min-width: 100%;
 
-    height: 31.25rem;
-    width: 29.6875rem;
-    
-
-    background-color: ${({ theme }) => theme["white-100"]};
-
-    h1 {
-        color: ${({ theme }) => theme["blue-700"]};
-
-        text-align: center;
-
-        margin-block: 3rem 3rem;
-    }
-
-    @media (max-width: 600px) {
-        height: 30rem;
-        width: 25rem;
-    }
-
-    @media (max-width: 432px) {
-        height: 27rem;
-        width: 20rem;
-    }
+    display: flex;
+    align-items: center;
+    justify-content: center;
 `
 
 export const FormContainer = styled.form`
-    width: 100%;
-
     display: flex;
     flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 1.875rem 0px;
 
-    padding: 0 1.5rem;
+    background-color: ${({ theme }) => theme["blue-500"]};
 
-    label {
-        color: ${({ theme }) => theme["blue-700"]};
-        font-size: 1.125rem;
-        font-weight: 700;
+    padding: 3rem;
+    border-radius: 8px;
 
+    max-width: 28.125rem;
+    width: 100%;
 
-        margin-bottom: 0.5rem;
+    h1 {
+        color: ${({ theme }) => theme.white};
+        font-size: 1.5rem;
+        font-weight: 600;
     }
+`
 
-    input {
-        border: 2px solid ${({ theme }) => theme["blue-700"]};
-        border-radius: 4px;
+export const SubContainerSign = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 0.2rem;
 
-        padding: 0.65rem;
-
-        margin-bottom: 2rem;
-    }
-
-    button {
-        padding: 0.65rem 1.75rem;
-        margin: 0 auto;
-
-        border: none;
-        border-radius: 8px;
-
-        background-color: ${({ theme }) => theme["blue-500"]};
-
+    p {
         color: ${({ theme }) => theme.white};
         font-weight: 400;
+    }
 
-        cursor: pointer; 
-
-        &:hover {
-            background-color: ${({ theme }) => theme["blue-700"]};
-
-            transition: 0.2s;
-        }
+    a {
+        color: ${({ theme }) => theme.white};
+        font-size: 0.875rem;
+        border-bottom: 2px solid ${({ theme }) => theme["white-500"]};
     }
 `
