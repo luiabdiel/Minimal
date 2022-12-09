1002bd00816b3265f603d9bee4f2e8769c43f419
Luiggi Abdiel <luiggiabdieldev@gmail.com>
2022-12-08 22:13:49 -0300

    New page login static

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,5 +1,20 @@
+import BackgroundSvg from '../../assets/AnimatedShape.svg'
+import { FormContainer, LoginContainer, LoginPage } from './styles'
+
 export function Login () {
     return (
-        <h1>Página de login</h1>
+        <LoginPage>
+            <img src={BackgroundSvg} alt="" />
+            <LoginContainer>
+                <h1>Acessar o sistema</h1>
+                <FormContainer>
+                    <label>Usuário</label>
+                    <input type="text" />
+                    <label>Senha</label>
+                    <input type="password" />
+                    <button type='button'>Entrar</button>
+                </FormContainer>
+            </LoginContainer>
+        </LoginPage>
     )
 }
---
+++ src/pages/Login/styles.ts
@@ -0,0 +1,86 @@
+import styled from "styled-components";
+
+export const LoginPage = styled.div`
+
+    height: 100vh;
+    width: 100vw;
+    
+    position: fixed;
+    
+    img {
+        background-size: cover;
+        background-position: center;
+
+        min-height: 100%;
+        min-width: 100%;
+    }
+`
+
+export const LoginContainer = styled.div`
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+
+    height: 31.25rem;
+    width: 29.6875rem;
+    
+
+    background-color: ${({ theme }) => theme["white-100"]};
+
+    h1 {
+        color: ${({ theme }) => theme["blue-700"]};
+
+        text-align: center;
+
+        margin-block: 3rem 3rem;
+    }
+`
+
+export const FormContainer = styled.form`
+    width: 100%;
+
+    display: flex;
+    flex-direction: column;
+
+    padding: 0 1.5rem;
+
+    label {
+        color: ${({ theme }) => theme["blue-700"]};
+        font-size: 1.125rem;
+        font-weight: 700;
+
+
+        margin-bottom: 0.5rem;
+    }
+
+    input {
+        border: 2px solid ${({ theme }) => theme["blue-700"]};
+        border-radius: 4px;
+
+        padding: 0.65rem;
+
+        margin-bottom: 2rem;
+    }
+
+    button {
+        padding: 0.65rem 1.75rem;
+        margin: 0 auto;
+
+        border: none;
+        border-radius: 8px;
+
+        background-color: ${({ theme }) => theme["blue-500"]};
+
+        color: ${({ theme }) => theme.white};
+        font-weight: 400;
+
+        cursor: pointer; 
+
+        &:hover {
+            background-color: ${({ theme }) => theme["blue-700"]};
+
+            transition: 0.2s;
+        }
+    }
+`
