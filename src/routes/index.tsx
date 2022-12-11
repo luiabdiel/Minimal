48d782e61678e9307e3f4bfea6099a7ae8b2bdfd
Luiggi Abdiel <luiggiabdieldev@gmail.com>
2022-12-10 21:41:37 -0300

    Refactor routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,21 +1,27 @@
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import { ProtectedLayout } from "../components/ProtectedLayout";
+import { AuthProvider } from "../contexts/AuthProvider";
 import { Home } from "../pages/Home";
 import { Login } from "../pages/Login";
-import { Register } from "../pages/Register";
 import { Transactions } from "../pages/Transactions";
 
 export function Router() {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path='/' element={<Home />} />
-                <Route path='/transactions' element={<Transactions />} />
-                <Route path='/register' element={<Register />} />
-                <Route path='/login' element={<Login />} />
+        <AuthProvider>
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<Home />} />
 
-                <Route path='/' element={<Navigate to='/' replace />} />
-                <Route path='*' element={<h1>Essa página não existe</h1>} />
-            </Routes>
-        </BrowserRouter>
+                    <Route path='/transactions' element={
+                        <ProtectedLayout children={<Transactions />} />}>
+                    </Route>
+
+                    <Route path='/login' element={<Login />} />
+
+                    <Route path='/' element={<Navigate to='/' replace />} />
+                    <Route path='*' element={<h1>Essa página não existe</h1>} />
+                </Routes>
+            </BrowserRouter>
+        </AuthProvider>
     )
 }
