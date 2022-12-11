8e37b4aa77d4220293bf6e55aca5db01b0843eab
Luiggi Abdiel <luiggiabdieldev@gmail.com>
2022-12-11 15:23:46 -0300

    new request api reqres

--- src/services/Api.ts
+++ src/services/Api.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
+import { getUserLocalStorage } from "../contexts/AuthProvider/util";
 
 export const Api = axios.create({
-    baseURL: "https://reqres.in/api/",
+    baseURL: "https://reqres.in/api",
 })
